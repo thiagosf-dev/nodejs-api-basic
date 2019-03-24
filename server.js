30805e0f4e1b46f97e53dea3009f8b99cf108412
Thiagosf-dev <thiagosf.dev@gmail.com>
2019-03-24 15:36:07 -0400

    Criando conexão com o banco de dados

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o app
 const app = express();
 
+// Iniciando o banco de dados
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true
+});
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send("Bem vindo");
 })
